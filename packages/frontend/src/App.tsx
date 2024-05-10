4f575c2312e849863c1e9d93149b6a0b7c9ed3b2
moshe <mosheyaku@gmail.com>
2024-05-10 18:19:53 +0300

    Added react-router and created an initial chat page

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,19 +1,26 @@
 import './normalize.css';
 import './App.scss';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 import Footer from './features/Footer';
 import Home from './features/Home';
 import Navbar from './features/Navbar';
+import ChatApp from './features/Messages/Chat/Message';
 
 function App() {
   return (
-    <>
+    <>    
+    <Router>
       <Navbar />
       {/* TODO: Add react-router */}
       <main style={{ flexGrow: 1, maxWidth: '400px', alignSelf: 'center' }}>
-        <Home />
+      <Routes>
+            <Route path="/chat" element={<ChatApp />} />
+          </Routes>
       </main>
       <Footer />
+      </Router>
     </>
   );
 }
---
+++ packages/frontend/src/features/Messages/Chat/Message/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ChatApp() {
+  return <div>Welcome to the Chat Page!</div>;
+}
+
+export default ChatApp;
