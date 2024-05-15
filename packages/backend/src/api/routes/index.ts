e46606a1bfffe25d28bcbe1dfea38c38e7d7ab70
JasonMan34 <JasonErlMan@gmail.com>
2024-05-16 00:03:37 +0300

    add match router

--- packages/backend/src/api/routes/index.ts
+++ packages/backend/src/api/routes/index.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
 import { authRouter } from './auth.js';
+import { matchesRouter } from './matches.js';
 import { usersRouter } from './users.js';
 
 const indexRouter = Router();
 
 indexRouter.use('/auth', authRouter);
 indexRouter.use('/users', usersRouter);
+indexRouter.use('/matches', matchesRouter);
 
 export { indexRouter };
