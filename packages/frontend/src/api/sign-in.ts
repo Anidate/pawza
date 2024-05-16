c66f70d89a656536729126f29b935ff50f69f4db
JasonMan34 <JasonErlMan@gmail.com>
2024-05-16 20:02:23 +0300

    /auth/signin

--- packages/frontend/src/api/sign-in.ts
+++ packages/frontend/src/api/sign-in.ts
@@ -2,4 +2,4 @@ import { apiClient } from './base';
 
 // TODO: Response interface
 
-export const signIn = (data: { email: string; password: string }) => apiClient.post('/sign-in', data);
+export const signIn = (data: { email: string; password: string }) => apiClient.post('/auth/sign-in', data);
