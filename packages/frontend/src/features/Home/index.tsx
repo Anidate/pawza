3b9db117c47b8b7e91c856859c082251adc532a2
moshe <mosheyaku@gmail.com>
2024-05-16 20:54:07 +0300

    add dummy user fields for home page

--- packages/frontend/src/features/Home/index.tsx
+++ packages/frontend/src/features/Home/index.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import GuyWithDog from '@src/assets/guy_with_dog.webp';
 import WomanWithDog from '@src/assets/woman_with_dog.jpg';
 import { useState } from 'react';
@@ -8,6 +8,22 @@ import PawButton from './PawButton';
 
 function Home() {
   const [guy, setGuy] = useState(true);
+
+  // Dummy data for two users
+  const userData = [
+    {
+      name: "John",
+      age: 35,
+      description: "Dog lover and outdoor enthusiast.",
+      image: GuyWithDog,
+    },
+    {
+      name: "Emily",
+      age: 28,
+      description: "Animal rescuer with a passion for hiking.",
+      image: WomanWithDog,
+    }
+  ];
 
   return (
     <Box
@@ -21,8 +37,35 @@ function Home() {
       flexDirection="column"
       justifyContent="end"
     >
-      <ImageCard src={GuyWithDog} style={{ display: !guy ? 'none' : '' }} />
-      <ImageCard src={WomanWithDog} style={{ display: guy ? 'none' : '' }} />
+      {/* Display user information of the current user */}
+      {guy && (
+        <>
+          <Typography variant="h4" align="center" gutterBottom>
+            {userData[0].name}, {userData[0].age}
+          </Typography>
+          <Typography variant="body1" align="center" gutterBottom>
+            {userData[0].description}
+          </Typography>
+
+          {/* ImageCard for the current user */}
+          <ImageCard src={userData[0].image} />
+        </>
+      )}
+
+      {/* Display user information of the other user */}
+      {!guy && (
+        <>
+          <Typography variant="h4" align="center" gutterBottom>
+            {userData[1].name}, {userData[1].age}
+          </Typography>
+          <Typography variant="body1" align="center" gutterBottom>
+            {userData[1].description}
+          </Typography>
+
+          {/* ImageCard for the other user */}
+          <ImageCard src={userData[1].image} />
+        </>
+      )}
 
       <Box display="flex" flexDirection="row" justifyContent="space-between" px={4}>
         <PawButton color="red" onClick={() => setGuy((p) => !p)} />
