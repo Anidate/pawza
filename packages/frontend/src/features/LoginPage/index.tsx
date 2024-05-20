3119ca0696a9b4186326f23a4a09d3144a4fd5f5
JasonMan34 <JasonErlMan@gmail.com>
2024-05-20 16:59:43 +0300

    woops

--- packages/frontend/src/features/LoginPage/index.tsx
+++ packages/frontend/src/features/LoginPage/index.tsx
@@ -34,21 +34,8 @@ export default function Login() {
     isSuccess,
     isError,
   } = useMutation({
-    // mutationFn: (data: { email: string; password: string }) => loginApiCall(data),
-
-    mutationFn(data: { email: string; password: string }) {
-      return loginApiCall(data);
-    },
+    mutationFn: (data: { email: string; password: string }) => loginApiCall(data),
   });
-
-  // Invoke the useMutation function
-  useMutation(
-    // Open an object definition
-    {
-      // The key mutationFn has the value:
-      mutationFn: (data: { email: string; password: string }) => loginApiCall(data),
-    },
-  );
 
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
