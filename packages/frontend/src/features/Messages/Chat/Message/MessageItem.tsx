ed6abbedb7bdae317c2a1f3c257d8526720b4bac
JasonMan34 <JasonErlMan@gmail.com>
2024-06-09 14:56:04 +0300

    some css

--- packages/frontend/src/features/Messages/Chat/Message/MessageItem.tsx
+++ packages/frontend/src/features/Messages/Chat/Message/MessageItem.tsx
@@ -6,26 +6,33 @@ interface MessageItemProps {
   isCurrentUser: boolean;
 }
 
-const MessageItem: React.FC<MessageItemProps> = ({ content, timestamp, isCurrentUser }) => {
-  return (
-    <Box
-      sx={{
-        alignSelf: isCurrentUser ? 'flex-end' : 'flex-start',
-        maxWidth: '60%',
-        padding: '1rem',
-        borderRadius: '1rem',
-        backgroundColor: isCurrentUser ? '#DCF8C6' : '#FFF',
-        boxShadow: '0 1px 3px rgba(0,0,0,0.1)',
-        wordBreak: 'break-word',
-        marginBottom: '1rem',
-      }}
+const MessageItem: React.FC<MessageItemProps> = ({ content, timestamp, isCurrentUser }) => (
+  <Box
+    sx={{
+      alignSelf: isCurrentUser ? 'flex-end' : 'flex-start',
+
+      maxWidth: '60%',
+      padding: '0.5rem',
+      borderStartStartRadius: '1rem',
+      borderStartEndRadius: '1rem',
+      borderEndEndRadius: isCurrentUser ? 0 : '1rem',
+      borderEndStartRadius: isCurrentUser ? '1rem' : 0,
+      backgroundColor: isCurrentUser ? '#DCF8C6' : '#FFF',
+      boxShadow: '0 1px 3px rgba(0,0,0,0.1)',
+      wordBreak: 'break-word',
+      marginBottom: '1rem',
+    }}
+  >
+    <Typography variant="body1" textAlign="left">
+      {content}
+    </Typography>
+    <Typography
+      variant="caption"
+      sx={{ display: 'block', textAlign: isCurrentUser ? 'right' : 'left', marginTop: '0.5rem', color: '#888' }}
     >
-      <Typography variant="body1">{content}</Typography>
-      <Typography variant="caption" sx={{ display: 'block', textAlign: isCurrentUser ? 'right' : 'left', marginTop: '0.5rem', color: '#888' }}>
-        {new Date(timestamp).toLocaleString()}
-      </Typography>
-    </Box>
-  );
-};
+      {new Date(timestamp).toLocaleString()}
+    </Typography>
+  </Box>
+);
 
 export default MessageItem;
